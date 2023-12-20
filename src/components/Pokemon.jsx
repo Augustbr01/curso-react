6804002781453685b0d9f15624c8235a5892ed36
August <augustogatticorrea777@gmail.com>
2023-12-20 01:43:47 -0400

    feat: new folder structure

--- src/components/Pokemon.jsx
+++
@@ -1,6 +0,0 @@
-export const Pokemon = ( {name = 'Pikachu'} ) => {
-    return <h2>Nome do Pokemon: {name}</h2>
-  }
-  
-
- 
---
+++ src/components/Pokemon/index.jsx
@@ -0,0 +1,6 @@
+export const Pokemon = ( {name = 'Pikachu'} ) => (
+   <h2>Nome do Pokemon: {name}</h2>
+  )
+  
+
+ 
