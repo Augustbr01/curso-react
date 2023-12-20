b04b8ac7d7f83b24c61cb8dd0d9946a7fe31394f
August <augustogatticorrea777@gmail.com>
2023-12-20 01:21:05 -0400

    feat: Add props to Pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,4 +1,6 @@
-export const Pokemon = () => {
-    return <h2>Nome do Pokemon: Pikachu</h2>
+export const Pokemon = ( {name = 'Pikachu'} ) => {
+    return <h2>Nome do Pokemon: {name}</h2>
   }
   
+
+ 
