a029cd67f9fd5834563bba1253a82ba3443329ef
August <augustogatticorrea777@gmail.com>
2023-12-21 13:52:13 -0400

    feat: Home page and Pokemon styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,16 @@
 import {Pokemon} from "../../components/Pokemon"
+import PokemonLogo from '../../assets/images/pokemon-logo.png'
+import styles from './styles.module.css'
 
 export const Home = () => {
   return (
-    <div>
-    <h1>Meu Site de Pokemon</h1>
-    <Pokemon name="Pikachu"/>
-    <Pokemon name="Charmander"/>
-    <Pokemon name="Charizard"/>
-    <Pokemon name="Squirtle"/>
-    <Pokemon name="Ditto"/>
+    <div className={styles.container}>
+      <img className={styles.image} src={PokemonLogo} alt="Pokemon logo" />
+      <Pokemon name="Pikachu"/>
+      <Pokemon name="Charmander"/>
+      <Pokemon name="Charizard"/>
+      <Pokemon name="Squirtle"/>
+      <Pokemon name="Ditto"/>
     </div> 
  )
 }
